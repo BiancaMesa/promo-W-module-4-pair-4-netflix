653ba9954dc51df4b4ced58e4bbcdf3edbb3d47e
mloherr <herraizmaria08@gmail.com>
2024-04-10 11:26:30 +0200

    changed data origin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -8,52 +8,50 @@ server.use(cors());
 server.use(express.json());
 
 // Nos conectamos a la base de datos
-async function getDBConnection(){
+async function getDBConnection() {
   const connection = await mysql.createConnection({
-      //Siempre tenemos que poner estos datos, y consultamos cómo rellenarlos en MySQL Workbench 
-      host: "127.0.0.1", 
-      user: "root",
-      password: "SheCodes0",
-      database: "Netflix",
+    //Siempre tenemos que poner estos datos, y consultamos cómo rellenarlos en MySQL Workbench
+    host: 'sql.freedb.tech',
+    user: 'freedb_admin_team4',
+    password: 'W4Vtbet*dv9MrkK',
+    database: 'freedb_netflix',
   });
 
-  //Nos conectamos 
+  //Nos conectamos
   connection.connect();
   return connection;
 }
 
-//Establecer el puerto que queremos usar 
+//Establecer el puerto que queremos usar
 // init express aplication
 const serverPort = 6001;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-//Creamos el endpoint 
-server.get("/movies", async (req, res) => {
-
+//Creamos el endpoint
+server.get('/movies', async (req, res) => {
   try {
-      const connection = await getDBConnection();
+    const connection = await getDBConnection();
 
-  const sql = "SELECT * FROM movies";
+    const sql = 'SELECT * FROM movies';
 
-  // Hacemos la consulta
-  const [moviesResult] = await connection.query(sql);
-  console.log(moviesResult);
+    // Hacemos la consulta
+    const [moviesResult] = await connection.query(sql);
+    console.log(moviesResult);
 
-  //Cerrar la conexión con la base de datos
-  connection.end();
+    //Cerrar la conexión con la base de datos
+    connection.end();
 
-  //Devolvemos la respuesta, en este caso con un objeto que tenga el código de que todo ha ido bien, el status y un mensaje
-  res.status(200).json({
-      status: "success", 
-      message: moviesResult, 
-  });
+    //Devolvemos la respuesta, en este caso con un objeto que tenga el código de que todo ha ido bien, el status y un mensaje
+    res.status(200).json({
+      status: 'success',
+      message: moviesResult,
+    });
   } catch (error) {
-      res.status(500).json({
-          status: "error",
-          message: "Ha habido un error interno. Contacte suporte", 
-      });
+    res.status(500).json({
+      status: 'error',
+      message: 'Ha habido un error interno. Contacte suporte',
+    });
   }
-  
 });
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -4,15 +4,15 @@ const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   return fetch('//localhost:6001/movies')
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((data) => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return data;
+      return data.message;
     });
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
